b0adc3de56deeb53b52788ca7700ca7903110c6c
MichaelGallinago <michael.gallinago@gmail.com>
2024-11-18 23:37:54 +0500

    1.1.0

--- StructArraySourceGenerator/StringBuilderExtensions.cs
+++ StructArraySourceGenerator/StringBuilderExtensions.cs
@@ -15,20 +15,20 @@ internal static class StringBuilderExtensions
     
     internal static StringBuilder AddCasesToGetValue(this StringBuilder builder, byte number)
     {
-        for (var i = 1; i < number; i++)
+        for (var i = 0; i < number; i++)
         {
             builder.Append("            ").Append(i).Append(" => _value").Append(i).AppendLine(",");
         }
-        return builder.AppendLine("            _ => _value0");
+        return builder.AppendLine("            _ => throw new System.IndexOutOfRangeException()");
     }
     
     internal static StringBuilder AddCasesToSetValue(this StringBuilder builder, byte number)
     {
-        for (var i = 1; i < number; i++)
+        for (var i = 0; i < number; i++)
         {
             builder.Append("                case ")
                 .Append(i).Append(": _value").Append(i).AppendLine(" = value; break;");
         }
-        return builder.AppendLine("                default: _value0 = value; break;");
+        return builder.AppendLine("                default: throw new System.IndexOutOfRangeException();");
     }
 }
--- StructArraySourceGenerator/StructArrayGenerator.cs
+++ StructArraySourceGenerator/StructArrayGenerator.cs
@@ -17,6 +17,8 @@ internal class StructArrayGenerator : IIncrementalGenerator
     private const string AttributeName = nameof(StructArrayAttribute);
     private const string FullAttributeName = nameof(StructArrayAttributes) + "." + AttributeName;
     
+    private static readonly StringBuilder FileBuilder = new();
+    
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
         IncrementalValueProvider<ImmutableArray<StructArrayData?>> provider = 
@@ -53,51 +55,58 @@ internal class StructArrayGenerator : IIncrementalGenerator
             .Where(item => item.HasValue)
             .Select(item => item!.Value);
         
-        Parallel.ForEach(filteredStructArrays, structArray => context.AddSource(
-            "StructArray.g.cs",
-            SourceText.From(CreateFileString(structArray), Encoding.UTF8)));
+        FileBuilder.Clear();
+        FileBuilder.AppendLine("using System.Runtime.CompilerServices;");
+        
+        Parallel.ForEach(filteredStructArrays, static structArray =>
+        {
+            string structArrayString = CreateStructArrayString(structArray);
+            lock (FileBuilder)
+            {
+                FileBuilder.AppendLine(structArrayString);
+            }
+        });
+        
+        context.AddSource("StructArray.g.cs", SourceText.From(FileBuilder.ToString(), Encoding.UTF8));
     }
 
-    private static string CreateFileString(StructArrayData structArray) => new StringBuilder().AppendLine(
-$$"""
-using System.Runtime.CompilerServices;
+    private static string CreateStructArrayString(StructArrayData structArray) => new StringBuilder().AppendLine(
+        $$"""
 
-namespace {{structArray.Namespace}}
-{
-    public struct {{structArray.Name}}<T>
-    {
-        public const byte Length = {{structArray.Size.ToString()}};
+          namespace {{structArray.Namespace}}
+          {
+              public struct {{structArray.Name}}<T>
+              {
+                  public const byte Length = {{structArray.Size.ToString()}};
+                  
+          """
+    ).AddFields(structArray.Size).AppendLine(
+        """
         
-"""
-        ).AddFields(structArray.Size).AppendLine(
-"""
-
-        public T this[int index]
-        {
-            get => GetValue(index);
-            set => SetValue(index, value);
-        }
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private T GetValue(int index) => index switch
-        {
-"""
-        ).AddCasesToGetValue(structArray.Size).AppendLine(
-"""
-        };
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void SetValue(int index, T value)
-        {
-            switch (index)
-            {
-"""
-        ).AddCasesToSetValue(structArray.Size).AppendLine(
-"""
+                public T this[int index]
+                {
+                    [MethodImpl(MethodImplOptions.AggressiveInlining)] get => GetValue(index);
+                    [MethodImpl(MethodImplOptions.AggressiveInlining)] set => SetValue(index, value);
+                }
+                
+                private T GetValue(int index) => index switch
+                {
+        """
+    ).AddCasesToGetValue(structArray.Size).AppendLine(
+        """
+                };
+                
+                private void SetValue(int index, T value)
+                {
+                    switch (index)
+                    {
+        """
+    ).AddCasesToSetValue(structArray.Size).AppendLine(
+        """
+                    }
+                }
             }
         }
-    }
-}
-"""
-        ).ToString();
+        """
+    ).ToString();
 }
